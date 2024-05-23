4291d0937846fa5a08a0c426fb6e23b881fb1c8d
hajew <hajewoong@gmail.com>
2024-05-23 19:58:30 +0900

    주석 수정

--- syndetector.py
+++ syndetector.py
@@ -4,25 +4,40 @@ from scapy.all import *
 from scapy.layers.inet import TCP, IP
 
 
-def get_syn_conn_status():
-    def packet_callback(packet):
+def get_syn_conn_status() -> int:
+    """
+    이 함수는 SYN 패킷을 감지하고 SYN 패킷의 개수를 반환합니다.
+    :return: SYN 패킷의 개수
+    """
+
+    def packet_callback(packet: Packet) -> bool:
+        """
+        이 함수는 SYN 패킷을 감지하고, 현재 날짜에 맞게 SYN패킷을 PCAP 파일로 저장합니다.
+        :param packet: 감지된 SYN 패킷
+        :return: SYN PACKET 이면 True, 아니면 False 반환
+        """
         if packet.haslayer(TCP) and packet[TCP].flags == 'S':
-            # with open('syn_packets_log.txt', 'a') as file:
-            #     file.write(f'{packet[IP].src} is sending a SYN packet to {packet[IP].dst}\n')
+            # pkt = PcapWriter(filedir + str(today.day) + ".pcap", append=True, sync=True)
+            # pkt.write(packet)
             return True
         return False
 
-    packets = sniff(filter="tcp", store=1, timeout=SleepTime)
+    packets = sniff(filter="tcp", store=1, timeout=SLEEPTIME)
     syn_packet_count = sum(1 for packet in packets if packet_callback(packet))
     return syn_packet_count
 
 
-def adjust_backlog_limit(index, increase):
+def adjust_backlog_limit(index: int, increase: bool) -> None:
+    """
+    이 함수는 백로그 크기를 조정합니다.
+    :param index: 백로그 크기를 조정할 인덱스
+    :param increase: Ture면 백로그 증가, False면 감소
+    """
     if increase:
-        adjusted_backlog = BacklogLimit[index]
+        adjusted_backlog = BACKLOG_LIMIT_ARRAY[index]
         action = 'increase'
     else:
-        adjusted_backlog = BacklogLimit[index]
+        adjusted_backlog = BACKLOG_LIMIT_ARRAY[index]
         action = 'decrease'
 
     with open('/proc/sys/net/ipv4/tcp_max_syn_backlog', 'w') as file:
@@ -33,12 +48,20 @@ def adjust_backlog_limit(index, increase):
         log_file.write(f'{action} backlog to {adjusted_backlog} at {now}\n')
 
 
-def get_synack_retries_status():
+def get_synack_retries_status() -> int:
+    """
+    이 함수는 SYNACK 재시도 횟수를 반환합니다.
+    :return: SYNACK 재시도 횟수
+    """
     with open('/proc/sys/net/ipv4/tcp_synack_retries', 'r') as file:
         return int(file.read().strip())
 
 
-def adjust_synack_retries(increase):
+def adjust_synack_retries(increase: bool) -> None:
+    """
+    이 함수는 SYNACK 재시도 횟수를 조정합니다.
+    :param increase: true면 SYNACK 재시도 횟수 증가, false면 감소
+    """
     synack_retries = get_synack_retries_status()
     action = 'increase' if increase else 'decrease'
 
@@ -55,12 +78,18 @@ def adjust_synack_retries(increase):
         log_file.write(f'{action} synack_retries to {synack_retries} at {now}\n')
 
 
-def get_syn_cookie_status():
+def get_syn_cookie_status() -> int:
+    """
+    :return: SYN 쿠키 상태
+    """
     with open('/proc/sys/net/ipv4/tcp_syncookies', 'r') as file:
         return int(file.read().strip())
 
 
-def adjust_syncookies(enable):
+def adjust_syncookies(enable: bool) -> None:
+    """
+    :param enable: true면 SYN 쿠키 활성화, false면 비활성화
+    """
     syncookies_status = '1' if enable else '0'
     action = 'enable' if enable else 'disable'
 
@@ -72,11 +101,16 @@ def adjust_syncookies(enable):
         log_file.write(f'{action} syncookies at {now}\n')
 
 
-def detect_packet(index):
+def detect_packet(index: int) -> int:
+    """
+    tcp소켓을 통해 SYN 패킷을 감지하고 백로그 크기, SYNACK 재시도 횟수, SYN 쿠키 상태를 조정합니다.
+    :return: index
+    :param index: 사용자가 지정한 백로그 크기 배열의 인덱스
+    """
     conn_count = get_syn_conn_status()
     print(f'Connection count: {conn_count}')
-    if conn_count >= BacklogLimit[index]:
-        if index < len(BacklogLimit) - 1:
+    if conn_count >= BACKLOG_LIMIT_ARRAY[index]:
+        if index < len(BACKLOG_LIMIT_ARRAY) - 1:
             index += 1
             adjust_backlog_limit(index, True)
         else:
@@ -89,7 +123,7 @@ def detect_packet(index):
         if get_syn_cookie_status() == 1:
             adjust_syncookies(False)
         else:
-            if get_synack_retries_status() != SYNACKretries:
+            if get_synack_retries_status() != SYNACK_RETRIES:
                 adjust_synack_retries(True)
             else:
                 if index > 0:
@@ -100,9 +134,13 @@ def detect_packet(index):
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-BacklogLimit = [int(x) for x in config['DEFAULT']['packetLimitArray'].split(',')]
-SleepTime = int(config['DEFAULT']['SleepTime'])
-SYNACKretries = int(config['DEFAULT']['SynAckRetries'])
+BACKLOG_LIMIT_ARRAY = [int(x) for x in config['DEFAULT']['BACKLOG_LIMIT_ARRAY'].split(',')]
+SLEEPTIME = int(config['DEFAULT']['SLEEPTIME'])
+SYNACK_RETRIES = int(config['DEFAULT']['SYNACK_RETRIES'])
+today = datetime.today()
+filedir = "./SYN/" + str(today.year) + "/" + str(today.month) + "/"
+if not os.path.exists(filedir):
+    os.makedirs(filedir)
 index = 0
 while True:
     index = detect_packet(index)
